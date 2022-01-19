10fd4431447ab659259055f6c3247c32fd8723aa
Sasha <sashapysmenny@gmail.com>
2022-01-19 23:25:35 +0200

    renamed security dir & updated design

---
+++ src/main/java/com/example/security/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.security;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private AccountRepo accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account acc = accountRepository.findAccountByEmail(username);
+        return new UserDatailsImpl(acc);
+    }
+}
