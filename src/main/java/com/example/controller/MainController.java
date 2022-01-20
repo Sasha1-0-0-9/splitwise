74d1393837e24db9fd9f53c35fb94ec58b5681c7
“Roman <romansavastru@gmail.com>
2022-01-20 03:59:05 +0200

    Fixed some errors 2

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,20 +1,12 @@
 package com.example.controller;
 
 import com.example.common.AuthenticationSystem;
-import com.example.repository.AccountRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
-    @Autowired
-    private AccountRepo accountRepository;
 
     @GetMapping("/")
     public String greeting(Model model) {
--- src/main/java/com/example/security/config/MvcConfig.java
+++ src/main/java/com/example/security/config/MvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
